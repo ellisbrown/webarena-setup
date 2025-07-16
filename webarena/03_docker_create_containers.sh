9d5c995b9dabbd341051c34edd51e187bdf126e5
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-07-16 05:47:20 +0000

    fix bug with starting wikipedia container

--- webarena/03_docker_create_containers.sh
+++ webarena/03_docker_create_containers.sh
@@ -11,7 +11,7 @@ docker create --name shopping -p $SHOPPING_PORT:80 shopping_final_0712
 docker create --name shopping_admin -p $SHOPPING_ADMIN_PORT:80 shopping_admin_final_0719
 docker create --name forum -p $REDDIT_PORT:80 postmill-populated-exposed-withimg
 docker create --name gitlab -p $GITLAB_PORT:$GITLAB_PORT gitlab-populated-final-port8023 /opt/gitlab/embedded/bin/runsvdir-start --env GITLAB_PORT=$GITLAB_PORT
-docker create --name wikipedia --volume=${WORKING_DIR}/wiki/:/data -p $WIKIPEDIA_PORT:80 ghcr.io/kiwix/kiwix-serve:3.3.0 wikipedia_en_all_maxi_2022-05.zim
+docker create --name wikipedia --volume=${ARCHIVES_LOCATION}/wiki/:/data -p $WIKIPEDIA_PORT:80 ghcr.io/kiwix/kiwix-serve:3.3.0 wikipedia_en_all_maxi_2022-05.zim
 
 # openstreetmap docker set up
 cd openstreetmap-website/
